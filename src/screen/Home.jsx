a6fa04af9952a89b365f869b6b66b895cb779488
M Abrar Khalid <abrarprince471@gmail.com>
2022-09-30 17:12:35 +0500

    home section

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -9,6 +9,18 @@ function Home() {
           <div className="home__container__img">
             <img src={Homesection} alt="" />
           </div>
+          <div className="home__container__content">
+            <div className="home__container__heading">
+              Full-Service Software Development Company
+            </div>
+            <div className="home__container__info">
+              Build world-class digital products with a team of design,
+              development and strategy experts. All in one place
+            </div>
+            <div className="home__container__btn">
+              <button>Let us support you</button>{" "}
+            </div>
+          </div>
         </div>
       </div>
     </>
