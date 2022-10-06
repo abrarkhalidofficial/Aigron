dd93523cc869df56be8f7c91af5acda7d7bc6821
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-10-06 18:58:26 +0500

    changings done

--- src/components/HeaderOverlayEntry.jsx
+++ src/components/HeaderOverlayEntry.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export function HeaderOverlayEntry({ icon, info, title }) {
+export function HeaderOverlayEntry({ icon, info, title, small }) {
   return (
     <div className="header__content__overlay__entry">
       <div className="header__content__overlay__entry__icon">{icon}</div>
-      <div className="header__content__overlay__entry__heading">{title}</div>
+      <div
+        className="header__content__overlay__entry__heading"
+        style={small ? { marginBottom: "0em" } : null}
+      >
+        {title}
+      </div>
       <div className="header__content__overlay__entry__info">{info}</div>
     </div>
   );
