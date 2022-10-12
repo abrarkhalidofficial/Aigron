91beeb92d2ddecae79d2a3dee704694227d11dd6
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-10-12 10:48:20 +0500

    api done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mailer = require("./mailer");
+
+// api config
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+
+app.use(express.json());
+app.use(cors());
+
+// db config
+
+// api routes
+app.post("/api/contact", (req, res) => {
+  console.log(req.body);
+  if (req.body) {
+    mailer("Contact Form", req.body);
+  }
+});
+
+// listen
+app.listen(port, () => console.log(`listening on localhost:${port}`));
---
+++ mailer.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+
+async function mailer(subject, data) {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  transporter.use(
+    "compile",
+    hbs({
+      viewEngine: {
+        extName: ".handlebars",
+        partialsDir: path.resolve(__dirname, "views"),
+        defaultLayout: false,
+      },
+      viewPath: path.resolve(__dirname, "views"),
+      extName: ".handlebars",
+    })
+  );
+  console.log(data);
+  let mailOptions = {
+    from: "mehfoozijaz786@gmail.com",
+    to: "mehfoozijaz786@gmail.com",
+    subject: subject,
+    template: "contact",
+    context: data,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log({ message: error });
+    } else {
+      console.log({
+        message: "Email sent: " + info.response,
+      });
+    }
+  });
+}
+
+module.exports = mailer;
